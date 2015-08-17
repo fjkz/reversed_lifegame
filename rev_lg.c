41f7e3b8afaf62a5e8c102d6e2e46bdc2a5a7b6a
Kazuho Fujii <kazuho.fujii@gmail.com>
2015-08-18 07:24:35 +0900

    clean

--- rev_lg.c
+++ rev_lg.c
@@ -3,18 +3,19 @@
 #include <string.h>
 #include <assert.h>
 
-#define DEAD  0
-#define ALIVE 1
-#define EMPTY 2
+#define DEAD     0
+#define ALIVE    1
+#define EMPTY    2
+#define NORESULT 3
 
-#define NUM_CANDIDATES_9CELLS_ALIVE 140 // 9C3 + 8C3
-#define NUM_CANDIDATES_9CELLS_DEAD  372 // 2^9 - 136
+#define NUM_CANDIDATES_CELL9_ALIVE 140 // 9C3 + 8C3
+#define NUM_CANDIDATES_CELL9_DEAD  372 // 2^9 - 136
 
 // List of the previous 3x3 cells paterns for each center cell state.
-static char **prev9cells_alive;
-static char **prev9cells_dead;
+static char **prev_cell9_alive;
+static char **prev_cell9_dead;
 
-int pop(int x)
+static int pop(int x)
 {
     x = x - ((x >> 1) & 0x55555555);
     x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
@@ -24,7 +25,7 @@ int pop(int x)
     return x & 0x0000003F;
 }
 
-int comp_pop(const void *a, const void *b)
+static int comp_pop(const void *a, const void *b)
 {
     int i = *(int *)a;
     int j = *(int *)b;
@@ -36,18 +37,18 @@ int comp_pop(const void *a, const void *b)
 void initialize()
 {
     // Memory allocation for 2D array.
-    prev9cells_alive = (char **)malloc(
-            sizeof(char *) * NUM_CANDIDATES_9CELLS_ALIVE);
+    prev_cell9_alive = (char **)malloc(
+            sizeof(char *) * NUM_CANDIDATES_CELL9_ALIVE);
 
-    for (int i = 0; i < NUM_CANDIDATES_9CELLS_ALIVE; i++) {
-        prev9cells_alive[i] = (char *)malloc(sizeof(char) * 9);
+    for (int i = 0; i < NUM_CANDIDATES_CELL9_ALIVE; i++) {
+        prev_cell9_alive[i] = (char *)malloc(sizeof(char) * 9);
     }
 
-    prev9cells_dead = (char **)malloc(
-            sizeof(char *) * NUM_CANDIDATES_9CELLS_DEAD);
+    prev_cell9_dead = (char **)malloc(
+            sizeof(char *) * NUM_CANDIDATES_CELL9_DEAD);
 
-    for (int i = 0; i < NUM_CANDIDATES_9CELLS_DEAD; i++) {
-        prev9cells_dead[i] = (char *)malloc(sizeof(char) * 9);
+    for (int i = 0; i < NUM_CANDIDATES_CELL9_DEAD; i++) {
+        prev_cell9_dead[i] = (char *)malloc(sizeof(char) * 9);
     } 
 
     // Counters for each state of the center cell.
@@ -76,37 +77,38 @@ void initialize()
         // The next cell is alive.
         if (sum == 3 || (sum == 4 && c[4] == ALIVE)) {
             for (int j = 0; j < 9; j++) {
-                prev9cells_alive[na][j] = c[j];
+                prev_cell9_alive[na][j] = c[j];
             }
             na++;
 
         // The next cell is dead.
         } else {
             for (int j = 0; j < 9; j++) {
-                prev9cells_dead[nd][j] = c[j];
+                prev_cell9_dead[nd][j] = c[j];
             }
             nd++;
         }
     }
 
-    assert(na == NUM_CANDIDATES_9CELLS_ALIVE);
-    assert(nd == NUM_CANDIDATES_9CELLS_DEAD);
+    assert(na == NUM_CANDIDATES_CELL9_ALIVE);
+    assert(nd == NUM_CANDIDATES_CELL9_DEAD);
 }
 
+struct field {
+    char *cell;
+    int nx;
+    int ny;
+};
+
 // Print field with PBM format.
-void print_field(const char *field, int nx, int ny)
+void print_field(struct field f)
 {
-    if (field == NULL) {
-        fprintf(stderr, "Argument field is NULL.\n");
-        return;
-    }
-
     printf("P1\n");
-    printf("%d %d\n", nx, ny);
+    printf("%d %d\n", f.nx, f.ny);
 
-    for (int j = 0; j < ny; j++) {
-        for (int i = 0; i < nx; i++) {
-            printf("%d ", field[i + nx * j]);
+    for (int j = 0; j < f.ny; j++) {
+        for (int i = 0; i < f.nx; i++) {
+            printf("%d ", f.cell[i + f.nx * j]);
         }
         printf("\n");
     }
@@ -143,16 +145,16 @@ static const int cy[9] = {-1, -1, -1,  0,  0,  0,  1,  1,  1};
 
 // Check the field is mathed to given 3x3 cells pattern.
 static inline
-int match9cells(const char *field, int nx, int ny,
-                const char *cells, int pos)
+int match_cell9(const char *cell, int nx, int ny,
+                const char *cell9, int pos)
 {
     int x = pos % nx;
     int y = pos / nx;
 
     for (int i = 0; i < 9; i++) {
         int p = get_pos(x + cx[i], y + cy[i], nx, ny);
-        char f = field[p];
-        if (f != EMPTY && f != cells[i]) {
+        char f = cell[p];
+        if (f != EMPTY && f != cell9[i]) {
             return UNMACHED;
         }
     }
@@ -162,44 +164,46 @@ int match9cells(const char *field, int nx, int ny,
 
 // Overwrite the field with the given 3x3 cells pattern.
 static inline
-char *overwrite9cells(char *field, int nx, int ny,
-                      const char *cells, int pos)
+void overwrite_cell9(char *cell, int nx, int ny,
+                     const char *cell9, int pos)
 {
     int x = pos % nx;
     int y = pos / nx;
 
     for (int i = 0; i < 9; i++) {
         int p = get_pos(x + cx[i], y + cy[i], nx, ny);
-        field[p] = cells[i];
+        cell[p] = cell9[i];
     }
-
-    return field;
 }
 
-static int count_called = 0;
+static unsigned long count_called = 0;
 
 // Find previos cells recursively.
 static
-char *_prev_field(const char *field, int nx, int ny,
-                  char *p_field, int pos,
-                  int *progress)
+void _prev_cell(struct field f, char *p_cell, int pos, int *progress)
 {
     count_called++;
 
     // Select candidates with the cell state.
-    char **prev9cells;
+    char **prev_cell9;
     int num_cand;
 
-    if (field[pos] == ALIVE) {
-        prev9cells = prev9cells_alive;
-        num_cand = NUM_CANDIDATES_9CELLS_ALIVE;
+    if (f.cell[pos] == ALIVE) {
+        prev_cell9 = prev_cell9_alive;
+        num_cand = NUM_CANDIDATES_CELL9_ALIVE;
     } else {
-        prev9cells = prev9cells_dead;
-        num_cand = NUM_CANDIDATES_9CELLS_DEAD;
+        prev_cell9 = prev_cell9_dead;
+        num_cand = NUM_CANDIDATES_CELL9_DEAD;
     }
 
-    const int size_p_field = sizeof(char) * nx * ny;
+    const int nx = f.nx;
+    const int ny = f.ny;
+    const int size_cell = sizeof(char) * nx * ny;
     const int pos_end = nx * ny - 1;
+
+    // Copy of the previous field candidate.
+    char p_cell_cpy[nx*ny];
+    memcpy(p_cell_cpy, p_cell, size_cell);
 
     // For all 3x3 cells pattern
     // whose center cell becomes the given field state.
@@ -210,96 +214,99 @@ char *_prev_field(const char *field, int nx, int ny,
     // Restart from the remembered candidate.
     for (int i = progress[pos]; i < num_cand; i++) {
         // Check the canditate macthes with the existed cells.
-        if (match9cells(p_field, nx, ny, prev9cells[i], pos) == UNMACHED) {
+        if (match_cell9(p_cell, nx, ny, prev_cell9[i], pos) == UNMACHED) {
             continue;
         }
 
-        // Copy of the previous field candidate.
-        char *p_field_ = (char *)malloc(size_p_field);
-        memcpy(p_field_, p_field, size_p_field);
-
-        p_field_ = overwrite9cells(p_field_, nx, ny, prev9cells[i], pos); 
+        overwrite_cell9(p_cell, nx, ny, prev_cell9[i], pos); 
 
         // The search is succeed if all the cells are covered.
         // Search from the next candidate at the next call.
         if (pos == pos_end) {
             progress[pos] = i + 1;
-            free(p_field);
-            return p_field_;
+            return;
         }
 
         // Find the previous pattern for the next cell.
-        p_field_ = _prev_field(field, nx, ny, p_field_, pos + 1, progress);
+        _prev_cell(f, p_cell, pos + 1, progress);
 
         // No pattern found. Try with the next candidate.
-        if (p_field_ == NULL) {
+        if (p_cell[0] == NORESULT) {
+            // Revert p_cell
+            memcpy(p_cell, p_cell_cpy, size_cell);
             continue;
         }
 
         // A suitable pattern was found.
         // Search from the this candidate at the next call.
         progress[pos] = i;
-        free(p_field);
-        return p_field_;
+        return;
     }
 
     // All candidate is not suitable.
     // Search from the first candidate at the next call
     // because the candidate of the previous position is changed.
     progress[pos] = 0;
-    free(p_field);
-    return NULL;
+    p_cell[0] = NORESULT;
+    return;
 }
 
 // Get a field pattern that becomes to
 // the given field pattern at the next step.
-char *prev_field(const char *field, int nx, int ny, int *progress)
+char *prev_cell(struct field f, int *progress)
 {
-    char *p_field = (char *)malloc(sizeof(char) * nx * ny);
+    int l = f.nx * f.ny;
+    char *p_cell = (char *)malloc(sizeof(char) * l);
 
-    for (int i = 0; i < nx * ny; i++) {
-        p_field[i] = EMPTY;
+    for (int i = 0; i < l; i++) {
+        p_cell[i] = EMPTY;
     }
 
-    p_field = _prev_field(field, nx, ny, p_field, 0, progress);
+    _prev_cell(f, p_cell, /* pos = */ 0 , progress);
 
-    return p_field;
+    return p_cell[0] == NORESULT ? NULL : p_cell;
 }
 
 // TODO: multi thead.
-char *ansistor_field(const char *field, int nx, int ny, int num_back)
+char *ansistor_field(struct field f, int back)
 {
     fprintf(stderr, "#");
 
     // Count the progress of searching.
-    int progress[nx * ny];
-    for (int i = 0; i < nx * ny; i++) {
+    int l = f.nx * f.ny;
+    int progress[l];
+    for (int i = 0; i < l; i++) {
         progress[i] = 0;
     }
 
+    struct field pf = {NULL, f.nx, f.ny};
+
     while (1) {
-        char *p_field = prev_field(field, nx, ny, progress);
+        char *p_cell = prev_cell(f, progress);
 
         // No suitable pattern.
-        if (p_field == NULL) {
+        if (p_cell == NULL) {
             fprintf(stderr, "\b");
-            return NULL;
+            return p_cell;
         }
 
-        if (num_back == 1) {
+        if (back == 1) {
             fprintf(stderr, "\n\n");
-            return p_field;
+            return p_cell;
         }
 
-        char *a_field = ansistor_field(p_field, nx, ny, num_back - 1);
-        free(p_field);
+        pf.cell = p_cell;
+
+        char *a_cell = ansistor_field(pf, back - 1);
+
+        free(p_cell);
 
         // Try the next previous field.
-        if (a_field == NULL) {
+        if (a_cell == NULL) {
             continue;
         }
 
-        return a_field;
+        return a_cell;
     }
 }
 
@@ -307,24 +314,32 @@ int main(int argc, char *argv[])
 {
     initialize();
 
-    char field[] = {
+    char cell[] = {
       0, 0, 0, 0, 0,
       0, 0, 0, 0, 0,
       0, 0, 1, 0, 0,
       0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0,
-      };
+      0, 0, 0, 0, 0
+    };
 
-    char *p_field = ansistor_field(field, 5, 5, 10);
+    struct field f = {
+      .cell = cell,
+      .nx = 5,
+      .ny = 5
+    };
 
-    if (p_field == NULL) {
+    int back = 10;
+
+    f.cell = ansistor_field(f, back);
+
+    if (f.cell == NULL) {
         fprintf(stderr, "No previous field pattern was found.\n");
         return 1;
     }
 
-    print_field(p_field, 5, 5);
+    print_field(f);
 
-    fprintf(stderr, "<_prev_field> is called %d times.\n", count_called);
+    fprintf(stderr, "<_prev_field> is called %lu times.\n", count_called);
 
     return 0;
 }
